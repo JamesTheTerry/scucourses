1c5f10b93cc5dfc220887952e14ef8d8dde44b82
nickmichaelp <nickmichaelp@Nicholas-Peacocks-MacBook-Pro.local>
2014-11-02 13:14:54 -0800

    Math Functions

--- apArrays.js
+++ apArrays.js
@@ -1,8 +1,54 @@
-<script>
 var math = ["MATH 9", "MATH 11", "MATH 12", "MATH 13", "MATH 14", "AMTH 106"];
 var coen = ["COEN 10", "COEN 11", "COEN 12", "CORE", "CORE"];
 var sci = [ "CHEM 11", "PHYS 31", "PHYS 32"];
 var core = ["CTW 1", "CTW 2", "COEN 19"];
 var replace = "CORE";
 
-</script>
+function CalcAB( ABScore, CRE ){
+	if(ABScore > 3){
+		var start = 2;
+		}
+	else{
+		if(CRE){
+			start = 1;
+		}
+		else{
+			start = 0;
+		}
+	}
+	for(var i = start; i < (start+3); i++){
+			writeln(math[i]);
+	
+	}
+}
+
+function CalcBC( BCScore, CRE ){
+	if(BCScore > 3){
+		start = 3;
+		}
+	else if(BCScore == 3){
+		start = 2;
+	}
+	else{
+		if(CRE){
+			start = 1;
+		}
+		else{
+			start = 0;
+		}
+	}
+	for(var i = start; i < (start+3); i++){
+			writeln(math[i]);
+	
+	}
+
+}
+
+function APcalc( ABScore, BCScore, CRE ){
+	if(BCScore >= 3){
+		CalcBC( BCScore, CRE );
+	}
+	else{
+		CalcAB( ABScore, CRE );
+	}
+}
