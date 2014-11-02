720b0e00b9188959a9c43e22a4a3afd6da1d432c
Tracey-Acosta <teadp@hotmail.com>
2014-11-02 11:38:07 -0800

    Revert "added arrays"
    This reverts commit 2b927206343c1e5f67de4eb1d18ba43489fe2701.

--- apArrays.js
+++
@@ -1,8 +0,0 @@
-<script>
-var math = ["MATH 9", "MATH 11", "MATH 12", "MATH 13", "MATH 14", "AMTH 106"];
-var coen = ["COEN 10", "COEN 11", "COEN 12", "CORE", "CORE"];
-var sci = [ "CHEM 11", "PHYS 31", "PHYS 32"];
-var core = ["CTW 1", "CTW 2", "COEN 19"];
-var replace = "CORE";
-
-</script>
