7c5ca2bd9eaa3ada761a889fbc0865d311864479
JamesTheTerry <xana33@gmail.com>
2014-11-03 09:24:15 -0800

    again again

--- SCUCoursesTrial1/jamesOnly.js
+++ SCUCoursesTrial1/jamesOnly.js
@@ -23,3 +23,48 @@ var sci = [ "CHEM 11", "PHYS 31", "PHYS 32"];
 var core = ["CTW 1", "CTW 2", "COEN 19"];
 var replace = "CORE";
 
+function COEN(){
+	//these will be the input variables
+	//set these it be the test scores
+	var APCompSci;
+	var IBCompSci;
+	
+	if (document.getElementById('radio40').checked) {
+		APCompSci = document.getElementById('radio40').value;
+  	} else if (document.getElementById('radio41').checked) {
+	  	APCompSci = document.getElementById('radio41').value;
+  	} else if (document.getElementById('radio42').checked) {
+	  	APCompSci = document.getElementById('radio42').value;
+  	} else if (document.getElementById('radio43').checked) {
+	  	APCompSci = document.getElementById('radio43').value;
+  	} else if (document.getElementById('radio44').checked) {
+	  	APCompSci = document.getElementById('radio44').value;
+  	} else if (document.getElementById('radio45').checked) {
+	  	APCompSci = document.getElementById('radio45').value;
+  	} else {
+	  	APCompSci = 0;
+  	}
+	document.getElementById("tester2").innerHTML="It worked: " + APCompSci;
+	
+	/*
+	//these will be the output variables, these are the classes that go in the matrix
+	var Fall1;
+	var Winter1;
+	var Spring1;
+	
+	if (APCompSci < 3 || IBCompSci < 6 ||(APCompSci == NA && IBCompSci == NA )){
+		//no credit follow the suggested plan
+		Fall1 = coen[0];
+		Winter1 = coen[1];
+		Spring1 = coen[2];
+	}else if (APCompSci == 3){
+		Fall1 = coen[1];
+		Winter1 = coen[2];
+		Spring1 = coen[3];
+	} else if (APCompSci >= 4 || IBCompSci >= 6){
+		Fall1 = coen[2];
+		Winter1 = coen[3];
+		Spring1 = coen[4];
+	}
+	*/	
+}
