67d3107082b040ace12d849d13cfc55510381839
nickmichaelp <nickmichaelp@Nicholas-Peacocks-MacBook-Pro.local>
2014-11-03 22:12:46 -0800

    Set up Math for transfer credits

--- SCUCoursesTrial2/jamesOnly.js
+++ SCUCoursesTrial2/jamesOnly.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	addCI();
 });
 
-var math = ["MATH 9", "MATH 11", "MATH 12", "MATH 13", "MATH 14", "AMTH 106"];
+var math = ["MATH 9", "MATH 11", "MATH 12", "MATH 13", "MATH 14", "AMTH 106", "CORE", "CORE", "CORE"];
 var coen = ["COEN 10", "COEN 11", "COEN 12", "CORE", "CORE"];
 var sci = [ "CHEM 11", "PHYS 31", "PHYS 32"];
 var core = ["CTW 1", "CTW 2", "COEN 19"];
@@ -68,8 +68,13 @@ function CalcFull(){
 	}
 	
 	//BC score of 3+ overides any AB score.  CRE is below all them
-	if(APCalcScoreBC >= 3) {
-		if(APCalcScoreBC > 3){
+	if(APCalcScoreBC == 8){
+		start = 6;
+	} else if(APCalcScoreBC == 7){
+		start = 5;
+	} else if(APCalcScoreBC == 6){
+		start = 4;
+	} else if(APCalcScoreBC >= 3) {		if(APCalcScoreBC > 3){
 			start = 3;
 		} else if (APCalcScoreBC == 3) {
 			start = 2;
