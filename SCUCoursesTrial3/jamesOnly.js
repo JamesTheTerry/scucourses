870903756ae43415b91c2f174595ca51354e601a
JamesTheTerry <xana33@gmail.com>
2014-11-04 00:34:54 -0800

    i hate c i

--- SCUCoursesTrial3/jamesOnly.js
+++ SCUCoursesTrial3/jamesOnly.js
@@ -67,6 +67,29 @@ function CalcFull(){
 		APCalcScoreBC = 0;
 	}
 	
+	//checkbox settings stuff
+	var math11 = document.getElementById("check9").checked;
+	var math12 = document.getElementById("check10").checked;
+	var math13 = document.getElementById("check11").checked;
+	var math14 = document.getElementById("check12").checked;
+	var amth106 = document.getElementById("check13").checked;
+	
+	if (math11 == true && APCalcScoreBC < 3){
+		APCalcScoreBC = 3;
+	}
+	if (math12 == true){
+		APCalcScoreBC = 5;
+	}
+	if (math13 == true){
+		APCalcScoreBC = 6;
+	}
+	if (math14 == true){
+		APCalcScoreBC = 7;
+	}
+	if (amth106 == true){
+		APCalcScoreBC = 8;
+	}
+	
 	//BC score of 3+ overides any AB score.  CRE is below all them
 	if(APCalcScoreBC == 8){
 		start = 6;
@@ -94,12 +117,15 @@ function CalcFull(){
 	document.getElementById("a1").innerHTML = Fall0;
 	document.getElementById("a2").innerHTML = Winter0;
 	document.getElementById("a3").innerHTML = Spring0;
+	
+	addCI();
 }
 
 function SciCred(){
 	var APChem;
 	var APphysics;
 	var IBChem;
+	var Transfer32 = 0;
 		
 	//get the APChem value
 	if (document.getElementById('radio30').checked) {
@@ -156,8 +182,20 @@ function SciCred(){
 	  	IBChem = 0;
   	}
   	
-  	Transfer32 = 0;
-  	
+  	//let's set some goddamn checkboxes brother!
+  	var chem11 = document.getElementById("check1").checked;
+	var phys31 = document.getElementById("check2").checked;
+	var phys32 = document.getElementById("check3").checked;
+	
+	if (chem11 == true){
+		APChem = 5;
+	}
+	if (phys31 == true){
+		APphysics = 5;
+	}
+	if (phys32 == true){
+		Transfer32 = 1;
+	}  	  	
 	
 	//logic
 	if(APChem > 2 || IBChem > 5){
@@ -171,7 +209,7 @@ function SciCred(){
 	} else{
 		Winter2 = sci[1];
 	}
-	if(Transfer32){
+	if(Transfer32 == 1){
 		Spring2 = replace;
 	} else{
 		Spring2 = sci[2];
@@ -229,6 +267,22 @@ function COEN(){
 	  	IBCompSci = 0;
   	}
 	
+	//let's set some goddamn checkboxes brother!
+  	var coen10 = document.getElementById("check5").checked;
+	var coen11 = document.getElementById("check6").checked;
+	var coen12 = document.getElementById("check7").checked;
+	
+	if (coen10 == true  && APCompSci < 3){
+		APCompSci = 3;
+	}
+	if (coen11 == true){
+		APCompSci = 5;
+	}
+	if (coen12 == true){
+		APCompSci = 6;
+	}
+	
+	
 	//these will be the output variables, these are the classes that go in the matrix
 	var Fall1;
 	var Winter1;
@@ -261,8 +315,21 @@ function COEN(){
 }
 
 function CTW(){
-	transferCTW = 0; //have they transfered out of CTW
-	transfer19 = 0; // have they trnasfered out of COEN19
+	var transferCTW = 0; //have they transfered out of CTW
+	var transfer19 = 0; // have they trnasfered out of COEN19
+	
+	//checkboxes...
+	var coen19 = document.getElementById("check8").checked;
+	var ctw = document.getElementById("check14").checked;
+	
+	if (coen19 == true){
+		transfer19 = 1;
+	}
+	if (ctw == true){
+		transferCTW = 1;
+	}
+	
+	
 	if(transferCTW){
 		var Fall3 = replace;
 		var Winter3 = replace;
@@ -279,21 +346,38 @@ function CTW(){
 	document.getElementById("d1").innerHTML = Fall3;
 	document.getElementById("d2").innerHTML = Winter3;
 	document.getElementById("d3").innerHTML = Spring3;
+	
+	addCI();
 }
 
 function addCI(){
-	var Fall = document.getElementById("").innerHTML;//set Fall to be the Fall array
-	var Winter = document.getElementById("").innerHTML;
-	var Spring = document.getElementById("").innerHTML;
+	var Fall = [];
+	var Winter = [];
+	var Spring = [];
+	Fall[0] = document.getElementById("a1").innerHTML;
+	Fall[1] = document.getElementById("b1").innerHTML;
+	Fall[2] = document.getElementById("c1").innerHTML;
+	Fall[3] = document.getElementById("d1").innerHTML;
+	Winter[0] = document.getElementById("a2").innerHTML;
+	Winter[1] = document.getElementById("b2").innerHTML;
+	Winter[2] = document.getElementById("c2").innerHTML;
+	Winter[3] = document.getElementById("d2").innerHTML;
+	Spring[0] = document.getElementById("a3").innerHTML;
+	Spring[1] = document.getElementById("b3").innerHTML;
+	Spring[2] = document.getElementById("c3").innerHTML;
+	Spring[3] = document.getElementById("d3").innerHTML;
 
 	var flag = 9;
 	var i;
+	if (Winter[2] == "C&I 2"){
+		document.getElementById("tester").innerHTML = "active";
+	}
 	
 	for (i=0;i<4;i++){
 		if (Winter[i] == "CORE"){
 			flag = i;
 		}
-	}	
+	}
 	
 	//this means that there is no core in the winter
 	//and you will not add C&I
@@ -303,15 +387,57 @@ function addCI(){
 	
 	for (i=0;i<4;i++){
 		if (Fall[i] == "CORE"){
-			Fall[i]= "C&I1";//this needs to change the class in the matrix
-			Winter[flag] = "C&I2";//this needs to change the class in the matrix
+			Fall[i]= "C&I 1";//this needs to change the class in the matrix
+			Winter[flag] = "C&I 2";//this needs to change the class in the matrix
+			
+			if (flag == 0){
+				document.getElementById("a2").innerHTML = "C&I 2";
+			} else if (flag == 1) {
+				document.getElementById("b2").innerHTML = "C&I 2";
+			} else if (flag == 2) {
+				document.getElementById("c2").innerHTML = "C&I 2";
+			} else if (flag == 3) {
+				document.getElementById("d2").innerHTML = "C&I 2";
+			}
+			
+			if (i == 0){
+				document.getElementById("a1").innerHTML = "C&I 1";
+			} else if (i == 1) {
+				document.getElementById("b1").innerHTML = "C&I 1";
+			} else if (i == 2) {
+				document.getElementById("c1").innerHTML = "C&I 1";
+			} else if (i == 3) {
+				document.getElementById("d1").innerHTML = "C&I 1";
+			}
+			
 			return;			
 		}
 	}	
 	for (i=0;i<4;i++){
 		if (Spring[i] == "CORE"){
-			Spring[i]= "C&I2";//this needs to change the class in the matrix
-			Winter[flag] = "C&I1";//this needs to change the class in the matrix
+			Spring[i]= "C&I 2";//this needs to change the class in the matrix
+			Winter[flag] = "C&I 1";//this needs to change the class in the matrix
+			
+			if (flag == 0){
+				document.getElementById("a2").innerHTML = "C&I 1";
+			} else if (flag == 1) {
+				document.getElementById("b2").innerHTML = "C&I 1";
+			} else if (flag == 2) {
+				document.getElementById("c2").innerHTML = "C&I 1";
+			} else if (flag == 3) {
+				document.getElementById("d2").innerHTML = "C&I 1";
+			}
+			
+			if (i == 0){
+				document.getElementById("a3").innerHTML = "C&I 2";
+			} else if (i == 1) {
+				document.getElementById("b3").innerHTML = "C&I 2";
+			} else if (i == 2) {
+				document.getElementById("c3").innerHTML = "C&I 2";
+			} else if (i == 3) {
+				document.getElementById("d3").innerHTML = "C&I 2";
+			}
+			
 			return;			
 		}
 	}	
